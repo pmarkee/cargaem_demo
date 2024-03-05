b565c4945a345595cb3dc8fb4bf3451d335aa17c
Peter Marki <marpeter00@gmail.com>
2024-03-05 20:24:47 +0100

    Simulate weight transfer

--- Spring.gd
+++ Spring.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-const STIFFNESS = 5000
-const DAMPING = 500
-const GROUND_DISTANCE = 300
+@export var stiffness = 5000
+@export var damping = 500
+@export var rest_distance = 100
 
 var offset = 0
 var prev_offset = 0
@@ -10,7 +10,7 @@ var velocity = 0
 
 
 func _ready():
-	$RayCast2D.target_position.y = GROUND_DISTANCE
+	$RayCast2D.target_position.y = rest_distance
 
 
 func _physics_process(delta):
@@ -18,7 +18,7 @@ func _physics_process(delta):
 		prev_offset = offset
 		offset = ($RayCast2D.target_position - $RayCast2D.get_collision_point() * global_transform).length()
 		velocity = (offset - prev_offset) / delta
-		var spring_force = STIFFNESS * offset
-		var damping_force = DAMPING * velocity
+		var spring_force = stiffness * offset
+		var damping_force = damping * velocity
 		var force_to_add = Vector2.UP * (spring_force + damping_force)
-		get_parent().apply_central_force(force_to_add * delta)
+		get_parent().apply_force(force_to_add * delta, position)
---
+++ vehicle.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+const ACCELERATION_FORCE = 500
+const BRAKING_FORCE = 700
+
+
+func _physics_process(delta):
+	if !$Body/GroundContactRayCast.is_colliding():
+		# This ray cast is a stupid way of detecting ground contact but it will do
+		# We should detect ground contact of each wheel: ground distance <= wheel radius + spring max len
+		return
+
+	var force = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		force.x += ACCELERATION_FORCE
+	if Input.is_action_pressed("ui_select"):
+		force.x -= sgn($Body.linear_velocity.x) * BRAKING_FORCE
+	
+	var torque = 0.0
+	if force.x > 0:
+		# Accelerating - apply counter clockwise torque, lifting the front
+		torque -= 50000
+	elif force.x < 0:
+		# Braking - apply clockwise torque, lifting the rear
+		torque += 70000
+
+	$Body.apply_force(force)
+	$Body.apply_torque(torque)
+
+
+func sgn(n: float) -> int:
+	if n < 0:
+		return -1
+	elif n == 0:
+		return 0
+	return 1
