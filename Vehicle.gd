6b45eab23a251033c6398a3b101077c03ea6bfb5
Peter Marki <marpeter00@gmail.com>
2024-03-20 20:43:16 +0100

    (wip) engine modeling

--- Vehicle.gd
+++ Vehicle.gd
@@ -3,6 +3,30 @@ extends Node2D
 const ACCELERATION_FORCE = 500
 const BRAKING_FORCE = 700
 
+# Engine parameters
+@export var MOMENT_OF_INERTIA = 1.0
+@export var DRAG_COEFFICIENT = 0.5
+@export var ROLLING_RESISTANCE = 10
+@export var RPM_LIMIT = 6000
+
+var rpm = 1000
+var throttle = 0.0
+
+const torque_curve = {
+	1000: 650,
+	1500: 830,
+	2000: 980,
+	2500: 1110,
+	3000: 1220,
+	3500: 1290,
+	4000: 1340,
+	4500: 1380,
+	5000: 1360,
+	5500: 1300,
+	6000: 1220,
+	6500: 1150,
+}
+
 enum SuspensionVariant {
 	V1_SINGLERAY,
 	V1_MULTIRAY_ANGLE,
@@ -12,6 +36,7 @@ enum SuspensionVariant {
 
 @export var variant = SuspensionVariant.V1_SINGLERAY
 @export var wheelbase = 70
+@export var wheel_radius = 31 # TODO make sure it's consistent here and in suspension
 
 
 func _ready():
@@ -35,14 +60,21 @@ func _ready():
 
 
 func _physics_process(delta):
+	find_closest_key(torque_curve, 1800)
 	if !$Body/GroundContactRayCast.is_colliding():
 		# This ray cast is a stupid way of detecting ground contact but it will do
 		# We should detect ground contact of each wheel: ground distance <= wheel radius + spring max len
 		return
 
-	var force = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
-		force.x += ACCELERATION_FORCE
+		throttle = 1
+	else:
+		throttle = 0
+	
+	var engine_torque = calculate_engine_torque(delta)
+	var force = Vector2(engine_torque / wheel_radius, 0)
+
+	# TODO braking
 	if Input.is_action_pressed("ui_select"):
 		force.x -= sgn($Body.linear_velocity.x) * BRAKING_FORCE
 	
@@ -56,6 +88,34 @@ func _physics_process(delta):
 
 	$Body.apply_force(force)
 	$Body.apply_torque(torque)
+
+
+func calculate_engine_torque(delta: float) -> float:
+	var closest_rpm = find_closest_key(torque_curve, rpm)
+	var engine_torque = torque_curve[closest_rpm] * throttle
+	var resistance = DRAG_COEFFICIENT * $Body.linear_velocity.x + ROLLING_RESISTANCE
+	var net_torque = engine_torque - resistance
+	var angular_acceleration = net_torque / MOMENT_OF_INERTIA
+	rpm += angular_acceleration * delta
+	rpm = clamp(rpm, 0, RPM_LIMIT)
+	print(net_torque)
+	print(rpm)
+	return net_torque
+
+
+func find_closest_key(dict: Dictionary, target: int) -> int:
+	var closest_key = null
+	var closest_diff = -1
+	
+	var keys = dict.keys()
+	keys.sort()
+	for key in keys:
+		var diff = abs(key - target)
+		if closest_key == null || diff < closest_diff:
+			closest_key = key
+			closest_diff = diff
+
+	return closest_key
 
 
 func sgn(n: float) -> int:
